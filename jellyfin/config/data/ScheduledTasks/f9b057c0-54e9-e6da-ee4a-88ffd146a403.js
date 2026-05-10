966f9c4288a19f14786633edc9150a322ccc8ae3
dabtt6 <datp617@gmail.com>
2026-05-10 21:58:10 +0700

    auto update 2026-05-10 21:58:10

---
+++ jellyfin/config/data/ScheduledTasks/f9b057c0-54e9-e6da-ee4a-88ffd146a403.js
@@ -0,0 +1,1 @@
+{"StartTimeUtc":"2026-05-10T14:57:51.3595318Z","EndTimeUtc":"2026-05-10T14:57:53.9192298Z","Status":"Completed","Name":"Update Plugins","Key":"PluginUpdates","Id":"f9b057c054e9e6daee4a88ffd146a403"}
