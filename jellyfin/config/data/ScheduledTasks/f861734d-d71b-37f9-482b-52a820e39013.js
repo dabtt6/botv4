5fd2cab4a44106b53c666cef1c0a01d7a9afff5b
dabtt6 <datp617@gmail.com>
2026-05-11 11:58:56 +0700

    auto update 2026-05-11 11:58:56

--- jellyfin/config/data/ScheduledTasks/f861734d-d71b-37f9-482b-52a820e39013.js
+++ jellyfin/config/data/ScheduledTasks/f861734d-d71b-37f9-482b-52a820e39013.js
@@ -1,1 +1,1 @@
-{"StartTimeUtc":"2026-05-10T16:57:49.7583643Z","EndTimeUtc":"2026-05-10T16:57:50.2703032Z","Status":"Completed","Name":"Media Segment Scan","Key":"TaskExtractMediaSegments","Id":"f861734dd71b37f9482b52a820e39013"}
+{"StartTimeUtc":"2026-05-11T04:58:50.7613542Z","EndTimeUtc":"2026-05-11T04:58:51.2482422Z","Status":"Completed","Name":"Media Segment Scan","Key":"TaskExtractMediaSegments","Id":"f861734dd71b37f9482b52a820e39013"}
