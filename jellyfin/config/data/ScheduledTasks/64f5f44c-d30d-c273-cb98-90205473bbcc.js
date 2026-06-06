ad3c77a7936faad14e1159841d0ce151c6d353d4
dabtt6 <datp617@gmail.com>
2026-06-07 03:00:06 +0700

    auto update 2026-06-07 03:00:06

--- jellyfin/config/data/ScheduledTasks/64f5f44c-d30d-c273-cb98-90205473bbcc.js
+++ jellyfin/config/data/ScheduledTasks/64f5f44c-d30d-c273-cb98-90205473bbcc.js
@@ -1,1 +1,1 @@
-{"StartTimeUtc":"2026-06-05T20:00:00.0296756Z","EndTimeUtc":"2026-06-05T20:00:00.9917186Z","Status":"Completed","Name":"Generate Trickplay Images","Key":"RefreshTrickplayImages","Id":"64f5f44cd30dc273cb9890205473bbcc"}
+{"StartTimeUtc":"2026-06-06T20:00:00.0077103Z","EndTimeUtc":"2026-06-06T20:00:01.0403306Z","Status":"Completed","Name":"Generate Trickplay Images","Key":"RefreshTrickplayImages","Id":"64f5f44cd30dc273cb9890205473bbcc"}
