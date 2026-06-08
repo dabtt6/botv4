1f67fbf7932333134b3d66309e816787a2a71b34
dabtt6 <datp617@gmail.com>
2026-06-08 12:53:33 +0700

    auto update 2026-06-08 12:53:33

--- jellyfin/config/data/ScheduledTasks/3a025083-141d-3c17-dd96-d5f9b951287b.js
+++ jellyfin/config/data/ScheduledTasks/3a025083-141d-3c17-dd96-d5f9b951287b.js
@@ -1,1 +1,1 @@
-{"StartTimeUtc":"2026-06-08T05:47:08.5965028Z","EndTimeUtc":"2026-06-08T05:47:08.6210423Z","Status":"Completed","Name":"Clean up collections and playlists","Key":"CleanCollectionsAndPlaylists","Id":"3a025083141d3c17dd96d5f9b951287b"}
+{"StartTimeUtc":"2026-06-08T05:53:31.2120178Z","EndTimeUtc":"2026-06-08T05:53:31.2259467Z","Status":"Completed","Name":"Clean up collections and playlists","Key":"CleanCollectionsAndPlaylists","Id":"3a025083141d3c17dd96d5f9b951287b"}
--- jellyfin/config/data/ScheduledTasks/f9b057c0-54e9-e6da-ee4a-88ffd146a403.js
+++ jellyfin/config/data/ScheduledTasks/f9b057c0-54e9-e6da-ee4a-88ffd146a403.js
@@ -1,1 +1,1 @@
-{"StartTimeUtc":"2026-06-08T05:19:56.0134385Z","EndTimeUtc":"2026-06-08T05:19:58.8715693Z","Status":"Completed","Name":"Update Plugins","Key":"PluginUpdates","Id":"f9b057c054e9e6daee4a88ffd146a403"}
+{"StartTimeUtc":"2026-06-08T05:47:08.6020455Z","EndTimeUtc":"2026-06-08T05:47:11.4707781Z","Status":"Completed","Name":"Update Plugins","Key":"PluginUpdates","Id":"f9b057c054e9e6daee4a88ffd146a403"}
