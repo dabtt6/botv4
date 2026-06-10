a88051cd7225f5c7661313a802f82723be119785
dabtt6 <datp617@gmail.com>
2026-06-11 05:41:49 +0700

    auto update 2026-06-11 05:41:49

--- jellyfin/config/data/ScheduledTasks/7738148f-fcd0-7979-c7ce-b148e06b3aed.js
+++ jellyfin/config/data/ScheduledTasks/7738148f-fcd0-7979-c7ce-b148e06b3aed.js
@@ -1,1 +1,1 @@
-{"StartTimeUtc":"2026-06-10T10:40:23.5342663Z","EndTimeUtc":"2026-06-10T10:40:44.5453641Z","Status":"Completed","Name":"Scan Media Library","Key":"RefreshLibrary","Id":"7738148ffcd07979c7ceb148e06b3aed"}
+{"StartTimeUtc":"2026-06-10T22:41:24.5347379Z","EndTimeUtc":"2026-06-10T22:41:46.8138114Z","Status":"Completed","Name":"Scan Media Library","Key":"RefreshLibrary","Id":"7738148ffcd07979c7ceb148e06b3aed"}
