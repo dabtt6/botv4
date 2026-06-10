4c799415545bfeeb3c4b83af1277c0588d5f91af
dabtt6 <datp617@gmail.com>
2026-06-10 17:42:30 +0700

    auto update 2026-06-10 17:42:30

--- jellyfin/config/data/ScheduledTasks/31de9ce8-3b92-23d3-38c7-7b1a635e144b.js
+++ jellyfin/config/data/ScheduledTasks/31de9ce8-3b92-23d3-38c7-7b1a635e144b.js
@@ -1,1 +1,1 @@
-{"StartTimeUtc":"2026-06-10T04:41:24.5328792Z","EndTimeUtc":"2026-06-10T04:41:24.9148066Z","Status":"Completed","Name":"Optimize database","Key":"OptimizeDatabaseTask","Id":"31de9ce83b9223d338c77b1a635e144b"}
+{"StartTimeUtc":"2026-06-10T10:42:25.5338471Z","EndTimeUtc":"2026-06-10T10:42:25.8747014Z","Status":"Completed","Name":"Optimize database","Key":"OptimizeDatabaseTask","Id":"31de9ce83b9223d338c77b1a635e144b"}
