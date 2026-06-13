87c46dfea33a48b544a7e841dd609c86373c2b9c
dabtt6 <datp617@gmail.com>
2026-06-14 02:00:07 +0700

    auto update 2026-06-14 02:00:07

--- jellyfin/config/data/ScheduledTasks/4e6637c8-32ed-644d-1af3-370a2506e80a.js
+++ jellyfin/config/data/ScheduledTasks/4e6637c8-32ed-644d-1af3-370a2506e80a.js
@@ -1,1 +1,1 @@
-{"StartTimeUtc":"2026-06-11T19:00:00.0015861Z","EndTimeUtc":"2026-06-11T19:00:00.8177107Z","Status":"Completed","Name":"Extract Chapter Images","Key":"RefreshChapterImages","Id":"4e6637c832ed644d1af3370a2506e80a"}
+{"StartTimeUtc":"2026-06-13T19:00:00.0903588Z","EndTimeUtc":"2026-06-13T19:00:02.4530089Z","Status":"Completed","Name":"Extract Chapter Images","Key":"RefreshChapterImages","Id":"4e6637c832ed644d1af3370a2506e80a"}
